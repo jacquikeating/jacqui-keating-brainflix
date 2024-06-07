e1b0dd53874a48af3a7b4e3eddfde00c45a74a7c
jacquikeating <j.keating@live.ca>
2024-06-06 20:50:01 -0400

    added assets & put basic content into Header
    - moved all fonts, images, and icons from design package to assets folder
    - added <header>, logo, search bar, icons, upload button, and avatar to Header, all as part of the same component
    - May switch to subcomponents later

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
 function Header() {
     return (
-        <h1>Hi, I'm Header</h1>
+        <header className="header">
+            <img className="header__logo" src="/src/assets/images/BrainFlix-logo.svg" />
+            <div className="header__search-bar">
+                <input className="header__search-field" type="text" placeholder="Search" />
+                <img className="header__search-icon" src="/src/assets/images/search.svg" alt="Magnifying glass icon" />
+            </div>
+            <button className="header__upload-btn">
+                <img className="header__upload-icon" src="/src/assets/images/upload.svg" />Upload
+            </button>
+            <img className="header__avatar" src="/src/assets/images/Mohan-muruge.jpg" alt="Your avatar" />
+        </header>
     )
 }
 
