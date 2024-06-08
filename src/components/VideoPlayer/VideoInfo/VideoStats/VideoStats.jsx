6c1ffde71f354c52605cc09893b57e5a6dbc4643
jacquikeating <j.keating@live.ca>
2024-06-08 17:57:36 -0400

    video section mobile styling
    - completed video section styling according to mobile mockup, with the exception of video player controls (to be added later)

--- src/components/VideoPlayer/VideoInfo/VideoStats/VideoStats.jsx
+++ src/components/VideoPlayer/VideoInfo/VideoStats/VideoStats.jsx
@@ -1,15 +1,21 @@
 import './VideoStats.css';
 
 function VideoStats({ data }) {
+    data.timestamp = new Date(data.timestamp).toLocaleDateString('en-US');
+    
     return (
         <div className="video-stats">
             <div className="video-stats__section">
-                <p className="video-stats__stat video-stats__stat--channel">{data.channel}</p>
+                <p className="video-stats__stat video-stats__stat--channel">By {data.channel}</p>
                 <p className="video-stats__stat">{data.timestamp}</p>
             </div>
             <div className="video-stats__section">
-                <p className="video-stats__stat">{data.views}</p>
-                <p className="video-stats__stat">{data.likes}</p>
+                <p className="video-stats__stat">
+                    <img src="/src/assets/images/views.svg" className="video-stats__icon" />
+                    {data.views}</p>
+                <p className="video-stats__stat">
+                <img src="/src/assets/images/likes.svg" className="video-stats__icon" />
+                    {data.likes}</p>
             </div>
         </div>
     )
