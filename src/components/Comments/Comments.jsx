dc1ecb9899487956d80ffff2f39b548320b5dffd
jacquikeating <j.keating@live.ca>
2024-06-08 22:55:58 -0400

    mobile styles for new comment section
    - formatted new comment section
    - added comments counter at top of comments section

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -5,9 +5,11 @@ import './Comments.scss';
 
 function Comments() {
     let thisVideo = videoDetails[0];
+    console.log(thisVideo)
 
     return (
         <section className="comments">
+            <p className="comments__counter">{thisVideo.comments.length} Comments</p>
             <NewComment data={thisVideo} />
             <LatestComments data={thisVideo} />
         </section>
--- src/components/Comments/NewComment/NewComment.jsx
+++ src/components/Comments/NewComment/NewComment.jsx
@@ -5,10 +5,11 @@ function NewComment() {
         <div className="new-comment">
             <img className="new-comment__user-avatar" src="/src/assets/images/Mohan-muruge.jpg" />
             <div className="new-comment__right-subsection">
-                <label className="new-comment__label" for="new-comment__text">Join the Conversation</label>
-                <textarea className="new-comment__text" id="new-comment__text"></textarea>
+                <label className="new-comment__label">Join the Conversation
+                    <textarea className="new-comment__text" name="new-comment__text" placeholder="Add a new comment"></textarea>
+                </label>
                 <button className="new-comment__btn">
-                    <img src="/src/assets/images/upload.svg" className="new-comment__icon" alt="Speech bubble with plus sign" />
+                    <img src="/src/assets/images/add_comment.svg" className="new-comment__icon" alt="Speech bubble with plus sign" />
                     Comment
                 </button>
             </div>
