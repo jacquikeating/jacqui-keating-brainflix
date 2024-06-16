1512d511641716aa556ab3637d176f7221043c65
jacquikeating <j.keating@live.ca>
2024-06-16 18:58:23 -0400

    disabled old video switching

--- src/components/NextVideos/VideoPreview.jsx
+++ src/components/NextVideos/VideoPreview.jsx
@@ -3,31 +3,31 @@ import  videoDetails from '/src/data/video-details.json';
 
 function VideoPreview({ title, image, channel, id, setCurrentVid }) {
 
-    function switchVideo(e) {
-        let vidId;
-        /* Explanation: Video ID is stored as the ID of the <li>. 
-            This conditional ensures we get the <li>'s ID, 
-            even when clicking a descendant. */
-        switch (e.target.nodeName) {
-            case "LI":
-                vidId = e.target.id;
-                break;
-            case "IMG":
-                vidId = e.target.parentElement.id;
-                break;
-            case "DIV":
-                vidId = e.target.parentElement.id;
-                break;
-            case "P":
-                vidId = e.target.parentElement.parentElement.id;
-                break;
-        }
-        let targetVid = videoDetails.find((video) => video.id === vidId);
-        setCurrentVid(targetVid);
-    }
+    // function switchVideo(e) {
+    //     let vidId;
+    //     /* Explanation: Video ID is stored as the ID of the <li>. 
+    //         This conditional ensures we get the <li>'s ID, 
+    //         even when clicking a descendant. */
+    //     switch (e.target.nodeName) {
+    //         case "LI":
+    //             vidId = e.target.id;
+    //             break;
+    //         case "IMG":
+    //             vidId = e.target.parentElement.id;
+    //             break;
+    //         case "DIV":
+    //             vidId = e.target.parentElement.id;
+    //             break;
+    //         case "P":
+    //             vidId = e.target.parentElement.parentElement.id;
+    //             break;
+    //     }
+    //     let targetVid = videoDetails.find((video) => video.id === vidId);
+    //     setCurrentVid(targetVid);
+    // }
 
     return (
-        <li className="video-preview" id={id} onClick={switchVideo}>
+        <li className="video-preview" id={id}>
             <img className="video-preview__image" src={image} alt="Video thumbnail" />
             <div className="video-preview__text">
                 <p className="video-preview__title">{title}</p>
